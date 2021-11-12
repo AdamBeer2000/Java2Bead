4f3e444362e4db2ccafbee6987c7cc821d18c821
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-11-12 09:56:26 +0100

    ToDoObject

--- ToDo/src/sample/Controller.java
+++ ToDo/src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
+import sample.objects.ToDoObject;
+
+import java.util.ArrayList;
+
 public class Controller {
+    ArrayList<ToDoObject> todo_list = new ArrayList<>();
 }
---
+++ ToDo/src/sample/objects/ToDoObject.java
@@ -0,0 +1,40 @@
+package sample.objects;
+
+import sample.enums.Category;
+import sample.enums.Importance;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ToDoObject {
+
+    public Importance importance;
+    public Category category;
+    public String title;
+    public String description;
+    public Date deadline;
+    public Date start_date;
+    public boolean is_finished;
+
+    private SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); // ezt máshol felhasználhatjuk!
+
+    public ToDoObject()
+    {
+        this.title = "NO TITLE";
+        this.description = "";
+        this.importance = Importance.NOT_IMPORTANT;
+        this.start_date = new Date();
+        this.is_finished = false;
+    }
+
+    public ToDoObject(String in_title, String in_description, Date in_deadline, Category in_category, Importance in_importance)
+    {
+        this.title = in_title;
+        this.description = in_description;
+        this.category = in_category;
+        this.importance = in_importance;
+        this.start_date = new Date();
+        this.deadline = in_deadline;
+        this.is_finished = false;
+    }
+}
