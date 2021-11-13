ecfd407f82a90ae74382b2d1d23b243c1a47b367
justabread <44095081+justabread@users.noreply.github.com>
2021-11-13 16:18:47 +0100

    basic todo functionality done

--- ToDo/src/sample/Controller.java
+++ ToDo/src/sample/Controller.java
@@ -1,9 +1,40 @@
 package sample;
 
-import sample.objects.ToDoObject;
-
-import java.util.ArrayList;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    ArrayList<ToDoObject> todo_list = new ArrayList<>();
+
+    @FXML
+    private ListView<String> mainList;
+
+    @FXML
+    private TextField mainInput;
+
+    @FXML
+    private MenuItem mainContextDelete;
+
+    @FXML
+    private Button mainAddButton;
+
+    @FXML
+    void mainAddButton_Click(ActionEvent event) {
+        if(!mainInput.getText().isEmpty())
+        {
+            String todo = mainInput.getText();
+            mainList.getItems().add(todo);
+        }
+    }
+
+    @FXML
+    void mainContextDelete_DeleteTodo(ActionEvent event) {
+        int selectedItem = mainList.getSelectionModel().getSelectedIndex();
+        System.out.println(selectedItem);
+        mainList.getItems().remove(selectedItem);
+    }
 }
