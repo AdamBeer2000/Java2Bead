a020159d11aa24243d1e19c44f14cf03d9257a85
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-11-19 20:42:29 +0100

    update

--- ToDo/src/sample/Controller.java
+++ ToDo/src/sample/Controller.java
@@ -1,13 +1,22 @@
 package sample;
 
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.time.LocalTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Controller {
 
@@ -30,12 +39,59 @@ public class Controller {
     private Button addTodo;
 
     @FXML
+    private TextField loginUsername;
+
+    @FXML
+    private TextField loginPassword;
+
+    @FXML
+    private TextField signinUsername;
+
+    @FXML
+    private TextField signinPassword;
+
+    @FXML
+    private TextField signinEmail;
+
+    @FXML
     private TextField taskName;
 
     @FXML
     private TextArea description;
 
-    public void addPaneButtonEvent(MouseEvent mouseEvent) {
+    @FXML
+    private Pane loginUsernamePN;
+
+    @FXML
+    private Pane loginPasswordPN;
+
+    @FXML
+    private Pane signinUsernamePN;
+
+    @FXML
+    private Pane signinPasswordPN;
+
+    @FXML
+    private Pane signinEmailPN;
+
+    @FXML
+    private Text clock;
+
+    /*@FXML
+    public void update()
+    {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalTime currentTime = LocalTime.now();
+            //clock.setText(currentTime.getHour() + ":" + currentTime.getMinute() + ":" + currentTime.getSecond());
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }*/
+
+    public void addPaneButtonEvent(MouseEvent mouseEvent)
+    {
         defaultVBox.setDisable(true);
         addPane.setDisable(false);
         addPane.setVisible(true);
@@ -46,7 +102,8 @@ public class Controller {
         description.setText("");
     }
 
-    public void addTodoButtonEvent(MouseEvent mouseEvent) {
+    public void addTodoButtonEvent(MouseEvent mouseEvent)
+    {
 
         if(!taskName.getText().isEmpty())
         {
@@ -60,7 +117,8 @@ public class Controller {
         }
     }
 
-    public void cancelAddTodoButtonEvent(MouseEvent mouseEvent) {
+    public void cancelAddTodoButtonEvent(MouseEvent mouseEvent)
+    {
         defaultVBox.setDisable(false);
         addPane.setDisable(true);
         addPane.setVisible(false);
@@ -71,17 +129,86 @@ public class Controller {
         description.setText("");
     }
 
-    public void loginButtonEvent(MouseEvent mouseEvent) {
+    public void loginButtonEvent(MouseEvent mouseEvent)
+    {
         loginPane.setVisible(false);
         loginPane.setDisable(true);
         defaultVBox.setDisable(false);
         defaultVBox.setVisible(true);
     }
 
-    public void signinButtonEvent(MouseEvent mouseEvent) {
+    public void signinButtonEvent(MouseEvent mouseEvent)
+    {
         loginPane.setVisible(false);
         loginPane.setDisable(true);
         defaultVBox.setDisable(false);
         defaultVBox.setVisible(true);
+    }
+
+    public void loginUsernameChangeEvent(KeyEvent keyEvent)
+    {
+        if(loginUsername.getText().isEmpty())
+        {
+            loginUsernamePN.setStyle("-fx-background-color: RED");
+        }
+        else
+        {
+            loginUsernamePN.setStyle("-fx-background-color: GREEN");
+        }
+    }
+
+    public void loginPasswordChangeEvent(KeyEvent keyEvent)
+    {
+        if(loginPassword.getText().isEmpty())
+        {
+            loginPasswordPN.setStyle("-fx-background-color: #FF0000");
+        }
+        else
+        {
+            loginPasswordPN.setStyle("-fx-background-color: #008000");
+        }
+    }
+
+    public void signinEmailChangeEvent(KeyEvent keyEvent)
+    {
+        // A regex nem működik
+        //^[A-Z0-9+_.-]+@[A-Z0-9.-]+$
+        String emailRegex = "^[A-Z0-9+_.-]+@[A-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        Matcher matcher = pattern.matcher(signinEmail.getText());
+
+        if(!(matcher.matches()))
+        {
+            signinEmailPN.setStyle("-fx-background-color: #FF0000");
+        }
+        else
+        {
+            signinEmailPN.setStyle("-fx-background-color: #008000");
+        }
+    }
+
+    public void signinUsernameChangeEvent(KeyEvent keyEvent)
+    {
+        if(signinUsername.getText().isEmpty())
+        {
+            signinUsernamePN.setStyle("-fx-background-color: #FF0000");
+        }
+        else
+        {
+            signinUsernamePN.setStyle("-fx-background-color: #008000");
+        }
+    }
+
+    public void signinPasswordChangeEvent(KeyEvent keyEvent)
+    {
+        if(signinPassword.getText().length() < 8)
+        {
+            signinPasswordPN.setStyle("-fx-background-color: #FF0000");
+        }
+        else
+        {
+            signinPasswordPN.setStyle("-fx-background-color: #008000");
+        }
     }
 }
