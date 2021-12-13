7baac7bbc437ddf37a02cc97c92a9abe14a43a7e
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2021-12-13 21:33:14 +0100

    inviter pach

--- ToDo/src/sample/groups/Invite.java
+++ ToDo/src/sample/groups/Invite.java
@@ -6,21 +6,36 @@ public class Invite
 {
     int inviteId;//meghívás azonosító
     User inviter;//meghívó user
+
+    String InviterName;
+    String GroupName;
+
     Group group;//és grup adatai (ahova meghívtak)
+
 
     public Invite(int inviteId,String userName, int userId, String groupName, int groupId)
     {
         this.inviteId=inviteId;
         this.inviter=new User(userName,userId);
+        InviterName=userName;
         this.group=new Group(groupId,groupName);
+        GroupName=groupName;
     }
 
     public String getInviterName()
     {
-        return inviter.getUserName();
+        return InviterName;
     }
     public String getGroupName()
     {
-        return group.getName();
+        return GroupName;
+    }
+    public void setInviterName(String s)
+    {
+        InviterName=s;
+    }
+    public void setGroupName(String s)
+    {
+        GroupName=s;
     }
 }
