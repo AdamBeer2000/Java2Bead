7865a0df393bdb9ea10115443830e52288aadb5a
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2021-12-15 00:15:26 +0100

    asd

--- ToDo/src/sample/Controller.java
+++ ToDo/src/sample/Controller.java
@@ -410,6 +410,7 @@ public class Controller
                         builder.withDeadLine(deadlinePicker.getValue());
                     }
                     builder.withCategory(currentCategory);
+                    builder.withCategory(currentCategory);
                     sds.TService().addTodoToGroup(slum.getUserid(), groupToAddTodo.getId(), builder.Build());
                     refreshUI();
                     defaultVBox.setDisable(false);
